68d0b9a301fa42c0f6c085a1ec41342eb6f5a0bb
Oxddd <xiangdeou@gmail.com>
2023-01-14 22:49:17 -0500

    ++

---
+++ Scripts/main.js
@@ -0,0 +1,13 @@
+/*import { halfOf , multiply } from "./lib.js";
+console.log(halfOf(86));
+console.log(multiply(2,4));*/
+
+/*
+import doSomething from "./doSomething.js";
+doSomething();
+*/
+
+import { flag,touch } from "./validator.js";
+console.log(flag);
+touch();
+console.log(flag);
---
+++ Scripts/spread.js
@@ -0,0 +1,6 @@
+function userTopFriends(firstFriend, secondFriend, thirdFriends) {
+    console.log(firstFriend);
+    console.log(secondFriend);
+    console.log(thirdFriends);
+}
+userTopFriends('oxd',...['Alice', 'Bob', 'Michelle']);
